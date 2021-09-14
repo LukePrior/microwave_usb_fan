0190c4eda6522226c8efb122ac08c8d6696892c2
Luke Prior <22492406+LukePrior@users.noreply.github.com>
2021-09-14 15:18:12 +1000

    Update message.py

--- usbfan/message.py
+++ usbfan/message.py
@@ -9,8 +9,8 @@ from .protocol import Colour, Column, Message, MessageStyle, OpenTransition, \
 class TextMessage(Message):
     def __init__(self, text: str,
                  message_style: MessageStyle=MessageStyle.Anticlockwise,
-                 open_transition: OpenTransition=OpenTransition.LeftRight,
-                 close_transition: CloseTransition=CloseTransition.RightLeft):
+                 open_transition: OpenTransition=OpenTransition.RightLeft,
+                 close_transition: CloseTransition=CloseTransition.LeftRight):
         # Open the font, create a blank image and an ImageDraw object
         fnt = ImageFont.truetype(
             join(dirname(__file__), 'fonts', 'Hack-Regular.ttf'))
@@ -22,9 +22,6 @@ class TextMessage(Message):
 
         # Transpose the image
         img = img.transpose(Image.TRANSPOSE)
-        
-        # Mirror image
-        #img = ImageOps.mirror(img)
         
         # Flip image
         img = ImageOps.flip(img)
